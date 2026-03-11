d06682b90221058ae95e6db394a271f09ebb6fac
James Jacobsen <james@Jamess-MacBook-Pro.local>
2026-03-11 16:22:59 -0500

    Initial project structure: .NET API, Worker, Core, Infrastructure, React frontend

---
+++ backend/JobTracker.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace JobTracker.Core;
+
+public class Class1
+{
+
+}
---
+++ backend/JobTracker.Worker/Program.cs
@@ -0,0 +1,7 @@
+using JobTracker.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
