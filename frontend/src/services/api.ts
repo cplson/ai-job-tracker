7f784cc3ae9ce44cba821d3eafa9870422defe05
James Jacobsen <james@Jamess-MacBook-Pro.local>
2026-05-18 23:26:39 -0500

    made it so api service uses .env data to call the right site location

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import type { InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5001/api';
+// const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL ?? '/api';
 
 const api = axios.create({
   baseURL: API_URL,
